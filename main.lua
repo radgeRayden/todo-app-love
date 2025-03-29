d1605b59c30e9464ce673c4d2bc3a5cb158b87d0
Westerbly Snaydley <westerbly@gmail.com>
2025-03-28 23:59:04 -0500

    add fennel support

--- main.lua
+++ main.lua
@@ -1,4 +1,30 @@
+local fennel = require("lib.fennel").install { correlate = true, moduleName = "lib.fennel" }
 local live = require "live"
+
+local make_love_searcher = function(env)
+    return function(module_name)
+        local path = module_name:gsub("%.", "/") .. ".fnl"
+        if love.filesystem.getInfo(path) then
+            return function(...)
+                local code = love.filesystem.read(path)
+                return fennel.eval(code, { env = env }, ...)
+            end,
+                path
+        end
+    end
+end
+
+table.insert(package.loaders, make_love_searcher(_G))
+table.insert(fennel["macro-searchers"], make_love_searcher "_COMPILER")
+debug.traceback = fennel.traceback
+
+-- require "src.main"
+local function fennel_loadfile(path, _, env)
+    return function ()
+        return fennel.dofile(path, {filename = path, env = env})
+    end
+end
+
 
 ---@param str string
 local function pattern_escape(str)
@@ -18,6 +44,8 @@ function love.load(args)
                     "mousepressed",
                     "mousereleased",
                 },
+                loadfile = string.find(match, ".+%.fnl") and fennel_loadfile or loadfile,
+                require_patterns = {"%s.lua", "%s.fnl"}
             })
         end
     end
